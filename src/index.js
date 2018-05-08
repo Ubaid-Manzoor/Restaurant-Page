655d5889dedd7f82b058b4a35ad11a68044abf61
Ubaid Manzoor <37380824+Ubaid-Manzoor@users.noreply.github.com>
2018-05-08 18:43:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import {LoadHomePage,LoadHomePageContent} from '../dist/home.js';
-
 import {LoadAboutContent} from '../dist/about.js';
-
 import {EraseMainContent} from '../dist/mainContent.js';
-
 import {LoadMenu} from '../dist/menu.js';
-
 import {LoadContacts} from '../dist/contact.js';
 
 LoadHomePage(); 
-console.log("sad");
+
 (()=>{
     let Home_Button = document.getElementsByClassName('Home_button')[0];
     let About_Button = document.getElementsByClassName('About_button')[0];
@@ -17,15 +13,10 @@ console.log("sad");
     let Contact_Button = document.getElementsByClassName('Contact_buton')[0];
     const ResetMenuColor = ()=>{
         let AllButton = document.querySelectorAll('li');
-        console.log(AllButton);
         AllButton.forEach((button)=>{
             button.style.background = "#fff";
         });
     }
-    console.log(Home_Button);
-    console.log(About_Button);
-    console.log(Menu_Button);
-    console.log(Contact_Button);
     Home_Button.onclick = ()=>{
         EraseMainContent();
         ResetMenuColor();
